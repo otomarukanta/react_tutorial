badfb80afd57f34960107b7b3d232ad31da6a85d
otomarukanta <kanta208@gmail.com>
2015-06-16 07:56:16 +0900

    Introduce Node for post method

---
+++ app/routes/comments.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs')
+
+var comments = JSON.parse(fs.readFileSync('data/comments.json'));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comments));
+});
+
+module.exports = router;
